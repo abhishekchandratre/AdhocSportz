eabb830570caa68474882e83af492ba22cb77f2c
abhishek.chandratre <abhishek.chandratre@gmail.com>
2016-10-30 13:37:59 -0400

    Basic login, TODO event and view profile

---
+++ Core/forms.py
@@ -0,0 +1,67 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Sports, SportsType, UserInfo, UserSportsInterest
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+        return user
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=250)
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput,
+        strip=False,
+    )
+
+
+class UserInfoForm(forms.ModelForm):
+
+    class Meta:
+        model = UserInfo
+        fields = ('gender','birthDate','phoneNumber','oneLinerStatus')
+
+
+class SportsInterestForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        choices = kwargs.pop('choices')
+        super(SportsInterestForm, self).__init__(*args, **kwargs)
+
+        for sport_type in choices:
+            self.fields[sport_type] = forms.CharField(
+                label=sport_type,
+                widget=forms.TextInput(attrs={'readonly':'readonly'}),
+                required=False,
+            )
+            for sport in choices[sport_type]:
+                self.fields[sport_type + sport] = forms.BooleanField(
+                    required=False,
+                    label=sport,
+                )
+
+    def save(self,choices,userInfoObjId):
+        for sport_type in choices:
+            for sport in choices[sport_type]:
+                if (sport_type + sport) in self.data:
+                    sportObj = Sports.objects.get(sportName=sport)
+                    userSportObj = UserSportsInterest(sport_id=sportObj.id,userInfo_id=userInfoObjId)
+                    userSportObj.save()
+
---
+++ scripts/sport_fill.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+db=$1
+echo "This script would populate tables in $db ."
+echo "This would erase will the present contents from the database.[y/n]"
+read input
+if [ $input == 'n' ]; then
+	echo "Exiting..."
+	exit
+fi
+echo "Deleting core_sportstype"
+sqlite3 $db "DELETE FROM Core_sportstype;"
+sqlite3 $db "delete from sqlite_sequence where name='Core_sportstype';"
+echo "Inserting into core_sportstype"
+sqlite3 $db "Insert into Core_sportstype (categoryName) values ('Team Sports');"
+sqlite3 $db "Insert into Core_sportstype (categoryName) values ('Fitness');"
+sqlite3 $db "Insert into Core_sportstype (categoryName) values ('Games / Entertainment');"
+sqlite3 $db "Insert into Core_sportstype (categoryName) values ('Outdoor Activities');"
+sqlite3 $db "Insert into Core_sportstype (categoryName) values ('Action Sports');"
+sqlite3 $db "Insert into Core_sportstype (categoryName) values ('Playground / Gym');"
+sqlite3 $db "Insert into Core_sportstype (categoryName) values ('Water Sports');"
+sqlite3 $db "Insert into Core_sportstype (categoryName) values ('Grass / Feild Sports');"
+sqlite3 $db "Insert into Core_sportstype (categoryName) values ('Winter Sports');"
+sqlite3 $db "Insert into Core_sportstype (categoryName) values ('Olympic Sports');"
+
+echo "Deleting Core_sports"
+sqlite3 $db "DELETE FROM Core_sports;"
+sqlite3 $db "delete from sqlite_sequence where name='Core_sports';"
+echo "Inserting into Core_sports"
+sportsTypeID=`sqlite3 $db "Select id from Core_sportstype where categoryName='Team Sports'"`
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Baseball',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Cricket',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Rugby',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Soccer',$sportsTypeID);"
+
+sportsTypeID=`sqlite3 $db "Select id from Core_sportstype where categoryName='Fitness'"`
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Boxing',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Climbing',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Crossfit',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Gymnastics',$sportsTypeID);"
+
+sportsTypeID=`sqlite3 $db "Select id from Core_sportstype where categoryName='Games / Entertainment'"`
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Bowling',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Chess',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Dart',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Poker',$sportsTypeID);"
+
+sportsTypeID=`sqlite3 $db "Select id from Core_sportstype where categoryName='Outdoor Activities'"`
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Airsoft',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Archery',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Camping',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Hiking',$sportsTypeID);"
+
+sportsTypeID=`sqlite3 $db "Select id from Core_sportstype where categoryName='Action Sports'"`
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Basejump',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('BMX',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Motorbike',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Skating',$sportsTypeID);"
+
+sportsTypeID=`sqlite3 $db "Select id from Core_sportstype where categoryName='Playground / Gym'"`
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('3D Dodgeball',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Handball',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Over the time',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Quidditch',$sportsTypeID);"
+
+sportsTypeID=`sqlite3 $db "Select id from Core_sportstype where categoryName='Water Sports'"`
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Swimming',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Diving',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Rowing',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Surfing',$sportsTypeID);"
+
+sportsTypeID=`sqlite3 $db "Select id from Core_sportstype where categoryName='Grass / Feild Sports'"`
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Beach Tennis',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Gold',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Polo',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Tennis',$sportsTypeID);" 
+
+sportsTypeID=`sqlite3 $db "Select id from Core_sportstype where categoryName='Winter Sports'"`
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Skiing',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Snowboarding',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('SnowsHoeing',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Snowmobiling',$sportsTypeID);"
+
+sportsTypeID=`sqlite3 $db "Select id from Core_sportstype where categoryName='Olympic Sports'"`
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Badminton',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Curling',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Fencing',$sportsTypeID);"
+sqlite3 $db "Insert into Core_sports (sportName,sportType_id) values ('Judo',$sportsTypeID);"
