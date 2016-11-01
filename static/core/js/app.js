0dd6f5e1b01d61abacab126588414625499d39e8
sampath <sgunase2@uncc.edu>
2016-10-31 18:36:28 -0700

    Added new files

---
+++ static/core/js/app.js
@@ -0,0 +1,35 @@
+/**
+ * Created by sampu on 10/31/16.
+ */
+'use strict';
+
+var sportsApp = angular.module('sportsApp',[]);
+
+sportsApp.config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[{');
+    $interpolateProvider.endSymbol('}]}');
+});
+
+sportsApp.config([
+    '$httpProvider',function ($httpProvider) {
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    }
+]);
+
+sportsApp.controller('userControl',['$scope','$http', '$attrs',
+    function userControl ($scope,$http,$attrs) {
+        var userID = $attrs.something;
+        $http.get('/core/api/user/' + userID).success(function(data) {
+            $scope.userDetail = data;
+        });
+    }
+    ]);
+
+sportsApp.controller('eventControl',['$scope','$http',
+    function eventControl ($scope,$http) {
+        $http.get('/core/api/event/').success(function(data) {
+            $scope.events = data;
+        });
+    }
+    ]);
